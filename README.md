3dd6aec3713af0d9914a60cc29787e4b178de462
Saumil Shah <saumpsh@gmail.com>
2025-01-15 03:00:55 +0000

    Boot

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # osin1000
+
+* OpenSBI
+  * BIOS/UEFI for PCs
+  * Download OpenSBI: `curl -LO https://github.com/qemu/qemu/raw/v8.0.4/pc-bios/opensbi-riscv32-generic-fw_dynamic.bin`
+
+
+  
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xue
+
+# QEMU file path
+QEMU=qemu-system-riscv32
+
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+# -bios default: Use the default firmware (OpenSBI in this case).
+# -nographic: Start QEMU without a GUI window.
+# -serial mon:stdio: Connect QEMU's standard input/output to the virtual machine's serial port. 
+# Specifying mon: allows switching to the QEMU monitor by pressing Ctrl+A then C.
+# --no-reboot: If the virtual machine crashes, stop the emulator without rebooting (useful for debugging).
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
