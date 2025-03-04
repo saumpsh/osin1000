0d80c5e6c3f570fc798ff478baa5028552e365a6
Saumil <saumpsh@gmail.com>
2025-03-04 07:36:29 -0800

    application

--- run.sh
+++ run.sh
@@ -4,12 +4,18 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
-CC=clang
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+CC=/opt/homebrew/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build the shell (application)
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
 
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 # -bios default: Use the default firmware (OpenSBI in this case).
 # -nographic: Start QEMU without a GUI window.
